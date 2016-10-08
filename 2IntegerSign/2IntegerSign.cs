8ae2632973a2bc7929afa6cde49425407d23cd98
didoff <dlucky95@gmail.com>
2016-10-08 21:33:25 +0300

    Methods and debugging + first few

---
+++ 2IntegerSign/2IntegerSign.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2IntegerSign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            IntegerSign(n);
+        }
+
+        static void IntegerSign(int number)
+        {
+            if (number == 0)
+                Console.WriteLine("The number {0} is zero.", number);
+            else if (number > 0)
+                Console.WriteLine("The number {0} is positive.", number);
+            else
+                Console.WriteLine("The number {0} is negative.", number);
+        }
+    }
+}
---
+++ TRY/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TRY
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NewMethod();
+        }
+
+        private static void NewMethod()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
