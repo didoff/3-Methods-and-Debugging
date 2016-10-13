fac125e26d8ca5aee485bb781cc9ea2317366f81
didoff <dlucky95@gmail.com>
2016-10-13 03:55:38 +0300

    Multiply Evens and Odds - done

---
+++ 8MultiplyEvensByOdds/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8MultiplyEvensByOdds
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int number = Math.Abs(int.Parse(Console.ReadLine()));
+
+            Console.WriteLine(GetMultiplicationOfEvensAndOdds(number));
+        }
+
+
+        public static int GetMultiplicationOfEvensAndOdds(int n)
+        {
+            int evens = GetSumOfEvens(n);
+            int odds = GetSumOfOdds(n);
+            return evens * odds;
+        }
+
+        public static int GetSumOfOdds(int n)
+        {
+            int sum = 0;
+            while (n>0)
+            {
+                int lastDigit = n % 10;
+                if (lastDigit % 2 != 0)
+                {
+                    sum += lastDigit;
+                }
+
+                n /= 10;
+            }
+
+            return sum;
+        }
+
+        public static int GetSumOfEvens(int n)
+        {
+            int sum = 0;
+            while (n > 0)
+            {
+                int lastDigit = n % 10;
+                if (lastDigit % 2 == 0)
+                {
+                    sum += lastDigit;
+                }
+
+                n /= 10;
+            }
+
+            return sum;
+        }
+    }
+}
